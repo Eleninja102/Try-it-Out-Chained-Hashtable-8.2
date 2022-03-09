5cc025360f09bcd841bfc533b0edb02b593ef469
Coleton Watt <electricninja102@gmail.com>
2022-03-08 20:57:28 -0700

    Initial Commit

---
+++ Try it Out: Chained Hashtable 8.2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Try it Out: Chained Hashtable 8.2
+//
+//  Created by Coleton Watt on 3/8/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

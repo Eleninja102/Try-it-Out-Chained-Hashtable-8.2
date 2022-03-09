02acafb24d15173dec4183feea1059f6ca79fa40
Coleton Watt <electricninja102@gmail.com>
2022-03-08 21:24:31 -0700

    Completed assignment

--- Try it Out: Chained Hashtable 8.2/main.cpp
+++ Try it Out: Chained Hashtable 8.2/main.cpp
@@ -5,10 +5,18 @@
 //  Created by Coleton Watt on 3/8/22.
 //
 
+#include "ChainedHashtable.h"
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+int main() {
+    //ChainedHashtable<int>ht;
+    ChainedHashtable<int>ht(127);
+    for (int i = 0; i < 100; i++) {
+    ht.insert(rand() % 100000);
+    }
+    cout << ht << endl;
+
+    
 }
